b8a6f5c0bf74624c0f31ce1f3d9c8fc51c5214b8
jentxab-ai <j.entxab@gmail.com>
2026-06-09 22:52:46 +0330

    Update main.dart
    Connect login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'features/auth/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text("Loyalty App MVP"),
-        ),
-      ),
+      home: LoginPage(),
     );
   }
 }
